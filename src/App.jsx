df419406ef538eda87db67e3b665226ef271d73b
tarunfy <tarunsharma8920@gmail.com>
2022-12-17 18:48:21 +0530

    initialize the project and create folder structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="title">Hello world!</h1>
+      <h1 className="title text-4xl text-indigo-500">My title</h1>
+    </div>
+  );
+};
+
+export default App;
